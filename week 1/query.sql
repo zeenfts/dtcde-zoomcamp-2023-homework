289e4d48e0780ac8419acaba4dca61ed67a90ae8
zeenfts <bluemul07@gmail.com>
2023-01-29 03:18:58 +0000

    update prove of homework part a

--- week 1/query.sql
+++ week 1/query.sql
@@ -37,6 +37,7 @@ FROM '/etc/postgresql/taxi+_zone_lookup.csv'
 DELIMITER ','
 CSV HEADER;
 
+-- ### [3] How many taxi trips were totally made on January 15? ### --
 SELECT count (1) AS total_trips
 FROM green_trips 
 WHERE lpep_pickup_datetime >= '2019-01-15 00:00:00' 
@@ -44,16 +45,21 @@ WHERE lpep_pickup_datetime >= '2019-01-15 00:00:00'
     AND lpep_dropoff_datetime >= '2019-01-15 00:00:00' 
     AND lpep_dropoff_datetime < '2019-01-16 00:00:00';
 
+--  ######### [4] Which was the day with the largest trip distance? ######### --
 SELECT cast(lpep_pickup_datetime as date), MAX(trip_distance) as total_trip
     FROM green_trips
     WHERE lpep_pickup_datetime >= '2019-01-01' AND lpep_pickup_datetime < '2019-02-01'
     GROUP BY 1
-    ORDER BY 2 desc;
+    ORDER BY 2 desc
+    LIMIT 5;
 
+-- ### [5] In 2019-01-01 how many trips had 2 and 3 passengers? ### --
+-- (don't forget to change pass_count!) --
 select count(1) from green_trips
 where pass_count = 3 
     and cast(lpep_pickup_datetime as date)='2019-01-01';
 
+--  ### [6] For the passengers picked up in the Astoria Zone which was the drop up zone that had the largest tip? ### --
 with gpbmax as (
     select doloc, max(tip_amount) as largest_tip 
     from green_trips 
